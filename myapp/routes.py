fa05d14d37fd02225e80404d87f0bd0f9ba42a38
Ogundepo Odunayo <jude@voyancehq.com>
2021-01-20 23:44:25 +0100

    added a login form

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template
 from myapp import app
+from myapp.forms import LoginForm
+
 
 @app.route("/")
 @app.route("/index")
@@ -17,3 +19,8 @@ def index():
         }
     ]
     return render_template('index.html',title="Home",user=user,posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html',title="Log in",form=form)
