c8f9ca00584867b91fea968cf9f7b5c8712415fd
Ogundepo Odunayo <jude@voyancehq.com>
2021-01-22 23:07:22 +0100

    add logout button

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, flash, redirect,url_for
 from myapp import app
 from myapp.forms import LoginForm
+from myapp.models import User
+from flask_login import login_user, current_user,logout_user
 
 
 @app.route("/")
@@ -22,9 +24,20 @@ def index():
 
 @app.route('/login',methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('login'))
+        user = User.query.filter_by(username = form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('invalid sign in credentials')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
     return render_template('login.html',title="Log in",form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
