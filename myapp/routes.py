ef073ddab6bda59e144816ec5838ce4ea30225c7
Ogundepo Odunayo <jude@voyancehq.com>
2021-01-22 23:54:41 +0100

    add registration db

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,7 +1,7 @@
 from werkzeug.urls import url_parse
 from flask import render_template, flash, redirect,url_for, request
-from myapp import app
-from myapp.forms import LoginForm
+from myapp import app,db
+from myapp.forms import LoginForm, RegistrationForm
 from myapp.models import User
 from flask_login import login_user, current_user,logout_user, login_required
 
@@ -45,3 +45,18 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit(): 
+        user = User(username=form.username.data,email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Successfully setup your user')
+        return redirect(url_for('login'))
+    return render_template('register.html',title="Register",form=form)
