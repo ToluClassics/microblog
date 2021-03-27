0d4caed105dcd9420bf9665df9911f6f24db6107
Ogundepo Odunayo <ogundepoodunayo@gmail.com>
2021-03-27 01:48:23 +0100

    add followes model

--- myapp/forms.py
+++ myapp/forms.py
@@ -33,3 +33,14 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username',validators = [DataRequired()])
     about_me = TextAreaField('About me',validators = [Length(min=0, max=140)] )
     submit = SubmitField('Submit')
+
+    #use a constructor to collect the original username from the form
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:                            #check if the original username has changed from what it was
+            user = User.query.filter_by(username = username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username')
