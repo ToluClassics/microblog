6a5042686f522d20bbd4874c403d57972f5e24c8
Ogundepo Odunayo <ogundepoodunayo@gmail.com>
2021-03-27 01:06:58 +0100

    logging and debugging

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,3 +1,5 @@
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -11,4 +13,39 @@ migrate = Migrate(app,db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from myapp import routes, models
+#Check if application is not in debut mode and there is a mail server configured
+if not app.debug and app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USER_TLS']:
+            secure = ()
+        #set up mail handler for logging to an email address
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),          #host
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],                       # fake from address
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',              # to address
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                        backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Microblog startup')
+
+from myapp import routes, models, errors
+
+
+
+'''to run sample debugging server
+python -m smtpd -n -c DebuggingServer localhost:5000
+'''
